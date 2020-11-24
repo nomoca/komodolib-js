bbdcba073760a3d94eb26b8c76959df8ad480990
nomoca <autonomoussoftware@protonmail.com>
2020-11-24 22:40:59 +0100

    Debugging

--- build/transaction-builder.js
+++ build/transaction-builder.js
@@ -195,11 +195,10 @@ var data = function data(network, value, fee, outputAddress, changeAddress, utxo
 
     // If value > max spendable value: subtract fee from max.
     if(targets[0].value > _maxSpendBalance) {
-      targets[0].value = _maxSpendBalance - fee
+      targets[0].value = _maxSpendBalance - fee;
     }
 
     console.log('targets', targets, 'utxoListFormatted', utxoListFormatted, '_maxSpendBalance', _maxSpendBalance, 'value', value)
-                                                                            _maxSpendBalance    13106             value    13106
 
     // default coin selection algo blackjack with fallback to accumulative
     // make a first run, calc approx tx fee
